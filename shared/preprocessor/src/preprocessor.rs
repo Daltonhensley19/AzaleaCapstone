9392230136791d0b8d6b17c4e848436b5f81f573
daltonhensley19 <daltonhensley2000@gmail.com>
2023-08-03 01:18:15 -0400

    Fixed bug with preprocessor not correctly removing multilines
    This issue was two bugs, really. We can now remove multilines in
    multilines. The second bug was related to removing a multiline with a
    `/` inside, which caused the comment-removal loop to escape before it
    found the end of the multiline comment (this meant we had only
    partially removed the multiline in these cases!!!)

--- shared/preprocessor/src/preprocessor.rs
+++ shared/preprocessor/src/preprocessor.rs
@@ -82,16 +82,16 @@ impl Preprocessor {
 
         // Loop to remove multi-line comments by appending non-comments to `result` and skipping
         // commented characters
-        let mut chars = content.chars().peekable();
-        //let mut in_multi_comment = false;
+        let mut chars          = content.chars().peekable();
         let mut offset_in_file = 0;
         while let Some(ch) = chars.next()
         {
             offset_in_file += 1;
-
+	 
             // Then we are in a multi-line comment, so skip passed characters until we are not
             if ch == '/' && chars.peek() == Some(&'*')
             {
+		
                 // Skip until we reach the end of the file or multi-line comment
                 chars.next();
                 offset_in_file += 1;
@@ -101,10 +101,15 @@ impl Preprocessor {
 
                 // NOTE: Temp_chr is used to both advance the position in the file as well as see if we
                 // are handling a whitespace character. If whitespace, then append to `result` to
-                // preserve token placement in the file.
+                // preserve token placement in the file. The while-loop runs until we find a
+		// `*` and `/` in a row. We also ignore multiline comments in multiline.
                 let mut temp_chr = chars.next();
-                while temp_chr != Some('*') && chars.peek() != Some(&'/')
+                while  (temp_chr != Some('*') && chars.peek() != Some(&'/'))
+		    || (temp_chr == Some('*') && chars.peek() != Some(&'/'))
+		    || (temp_chr != Some('*') && chars.peek() == Some(&'/'))
                 {
+
+		    
                     // If we are trying to handle multi-line comment and we reach the end of the
                     // file, then that means the user forgot to terminate their comment! So, the
                     // comment will actually have no end! Just report as an error.
@@ -123,6 +128,7 @@ impl Preprocessor {
                             path = self.path
                         )));
                     }
+
                     offset_in_file += 1;
 
                     // Preserve whitespace to maintain span locations
@@ -131,11 +137,12 @@ impl Preprocessor {
                         result.push(temp_chr.unwrap());
                     }
 
+
                     // Advance to the next character
                     temp_chr = chars.next();
                 }
 
-                chars.next();
+		chars.next();
                 offset_in_file += 1;
             }
             else
@@ -157,7 +164,7 @@ impl Preprocessor {
 
         const VALID_PUNC: &[&str] = &[
             ";", ":", "_", ",", "(", ")", "{", "}", "+", "-", "*", "/", "%", "&", "|", "=", "<",
-            ">", "!", ".",
+            ">", "!", ".", "[", "]",
         ];
         const VALID_CONTROL: &[&str] = &["\n", "\t", "\r"];
 
